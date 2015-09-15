f8284cb83e2aacb4dbe44196e0fb95700fb65cf3
Brandon Kirby <brandon.kirby@gsa.gov>
2015-09-15 06:43:58 -0400

    Use the correct /sign namespace when pulling the securityToken out of the response xml.

--- server/authorizeAuthenticate.js
+++ server/authorizeAuthenticate.js
@@ -35,7 +35,7 @@ Meteor.methods({
 var getAuthorizeAuthenticateJson = function(xml) {
   var securityToken = xml.get("/soap:Envelope/soap:Body/ns2:AuthenticateResponse/securityToken", {
     "soap": "http://www.w3.org/2003/05/soap-envelope",
-    "ns2": "http://www.exchangenetwork.net/wsdl/register/1"
+    "ns2": "http://www.exchangenetwork.net/wsdl/register/sign/1"
   }).text();
   console.log("securityToken: " + securityToken);
   return { securityToken: securityToken };
@@ -44,8 +44,8 @@ var getAuthorizeAuthenticateJson = function(xml) {
 var getAuthenticateErrorJson = function(xml) {
   var fault = xml.get("/soap:Envelope/soap:Body/soap:Fault/soap:Detail/ns1:RegisterFault", {
     "soap": "http://www.w3.org/2003/05/soap-envelope",
-    "ns1": "http://www.exchangenetwork.net/wsdl/register/1",
-    "ns2": "http://www.exchangenetwork.net/wsdl/register/1"
+    "ns1": "http://www.exchangenetwork.net/wsdl/register/sign/1",
+    "ns2": "http://www.exchangenetwork.net/wsdl/register/sign/1"
   });
 
   var description = fault.get("description").text();
