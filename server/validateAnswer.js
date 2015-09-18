a764a9c74a35df603b02d365511917907df6c622
Brandon Kirby <brandon.kirby@gsa.gov>
2015-09-17 22:01:49 -0400

    Initial, untested version of validate answer call. Need network.

---
+++ server/validateAnswer.js
@@ -0,0 +1,23 @@
+Meteor.methods({
+  validateAnswer: function(props) {
+    return registerSignService({
+      props: props,
+      templateName: "validateAnswer",
+      success: getValidateAnswerJson
+    });
+  }
+});
+
+var getValidateAnswerJson = function(xml) {
+  var validAnswer = xml.get("/soap:Envelope/soap:Body/ns2:ValidateAnswerResponse/validAnswer", {
+    "soap": "http://www.w3.org/2003/05/soap-envelope",
+    "ns2": "http://www.exchangenetwork.net/wsdl/register/sign/1"
+  }).text();
+
+  var isValidAnswer = (validAnswer.lowercase() === "true");
+  
+  console.log("is valid answer: " + isValidAnswer);
+  
+  return { isValidAnswer: isValidAnswer };
+}
+
